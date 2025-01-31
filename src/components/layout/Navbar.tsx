d26d6eeb4907d21bf11f7e0b244b2bb0a4eeb6f4
Md Aminul Islam <mr.aminul118@gmail.com>
2025-01-31 07:53:19 +0600

    Navlink change

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -8,6 +8,18 @@ const Navbar = () => {
       <li>
         <Link href="/">Home</Link>
       </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href="/services">Services</Link>
+      </li>
+      <li>
+        <Link href="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link href="/contact">Contact</Link>
+      </li>
     </>
   );
   return (
